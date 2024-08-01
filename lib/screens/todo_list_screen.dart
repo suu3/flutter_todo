041884a59295f56fb446523fa7afe59341a1e5e4
gigilxx <haenaexx@naver.com>
2024-08-01 15:29:39 +0900

    FEAT : to-do create 기능 추가

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -20,71 +20,75 @@ class TodoListScreen extends ConsumerWidget {
           },
         ),
       ),
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 20,
-            ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.person_4,
-                  size: 60,
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Tasks',
-                      style: TextStyle(
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 16,
-                          letterSpacing: 1.2,
-                          height: 1),
-                    ),
-                    Text(
-                      'Personal',
-                      style: TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 36,
-                          height: 1.1),
-                    ),
-                  ],
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: todoList.length,
-              itemBuilder: (context, index) {
-                final todo = todoList[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 4.0, horizontal: 16.0),
-                  child: LabelCheckbox(
-                    label: todo.title,
-                    isChecked: todo.completed,
-                    onChanged: (value) {
-                      todoListNotifier.updateTodo(
-                        todo.id,
-                        completed: value ?? false,
-                      );
-                    },
-                    direction: Direction.left,
+      body: todoList.when(
+        data: (todos) => Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+                vertical: 20,
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.person_4,
+                    size: 60,
                   ),
-                );
-              },
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Tasks',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w400,
+                            fontSize: 16,
+                            letterSpacing: 1.2,
+                            height: 1),
+                      ),
+                      Text(
+                        'Personal',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 36,
+                            height: 1.1),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                itemCount: todos.length,
+                itemBuilder: (context, index) {
+                  final todo = todos[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 4.0, horizontal: 16.0),
+                    child: LabelCheckbox(
+                      label: todo.title,
+                      isChecked: todo.completed,
+                      onChanged: (value) {
+                        todoListNotifier.updateTodo(
+                          todo.id,
+                          completed: value ?? false,
+                        );
+                      },
+                      direction: Direction.left,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+        loading: () => const Center(child: CircularProgressIndicator()),
+        error: (error, stack) => Center(child: Text('Error: $error')),
       ),
     );
   }
