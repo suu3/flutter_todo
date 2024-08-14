9d11fd81fe237b4f00e3cc9717db78746654e7fe
chanw9503 <chanw9503@gmail.com>
2024-08-14 16:03:10 +0900

    Fix. todo_list temp 폴더 비교 후 삭제

--- lib/providers/todo_list.dart
+++ lib/providers/todo_list.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:flutter_todo/models/todo.dart';
+import 'package:flutter_todo/models/temp/todo.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -15,78 +15,22 @@ class TodoList extends _$TodoList {
   Future<List<Todo>?> getTodos(String userId) async {
     final supabase = Supabase.instance.client;
     try {
-      final response =
-          await supabase.from('tasks').select().eq('user_id', userId);
+      final response = await supabase
+          .from('tasks')
+          .select('*, checklist(*)')
+          .eq('user_id', userId);
+
       if (response.isEmpty) {
         return null;
       }
+
       final todos = (response as List)
-          .map((todoData) => Todo(
-                id: todoData['id'],
-                userId: todoData['user_id'],
-                categoryId: todoData['category_id'],
-                title: todoData['title'],
-                description: todoData['description'],
-                createdAt: DateTime.parse(todoData['created_at']),
-                updatedAt: DateTime.parse(todoData['updated_at'] as String? ??
-                    DateTime.now().toIso8601String()),
-                startedAt: DateTime.parse(todoData['started_at']),
-                endedAt: DateTime.parse(todoData['ended_at']),
-                completed: todoData['completed'],
-              ))
+          .map((taskJson) => Todo.fromJson(taskJson))
           .toList();
       return todos;
     } catch (e) {
       print(e);
       return null;
-    }
-  }
-
-  Future<void> addTodo(String title) async {
-    final supabase = Supabase.instance.client;
-    try {
-      var userId = 'ad5a4932-19e5-4f97-a4d8-29a6fa0e2c0b';
-      var categoryId = 'fb1778f3-838c-4c2c-8d27-454ef6993b13';
-      var description = 'test description';
-      var createdAt = DateTime.now().toIso8601String();
-      var updatedAt = DateTime.now().toIso8601String();
-      var startedAt = DateTime.now().toIso8601String();
-      var endedAt =
-          DateTime.now().add(const Duration(days: 7)).toIso8601String();
-      var completed = false;
-
-      final response = await supabase.from('tasks').insert({
-        'user_id': userId,
-        'category_id': categoryId,
-        'title': title,
-        'description': description,
-        'created_at': createdAt,
-        'updated_at': updatedAt,
-        'started_at': startedAt,
-        'ended_at': endedAt,
-        'completed': completed,
-      }).select();
-
-      if (response.isEmpty) {
-        return;
-      }
-
-      final newTodo = Todo(
-        id: response.first['id'],
-        userId: response.first['user_id'],
-        categoryId: response.first['category_id'],
-        title: response.first['title'],
-        description: response.first['description'],
-        createdAt: DateTime.parse(response.first['created_at']),
-        updatedAt: DateTime.parse(response.first['updated_at']),
-        startedAt: DateTime.parse(response.first['started_at']),
-        endedAt: DateTime.parse(response.first['ended_at']),
-        completed: response.first['completed'],
-      );
-
-      state = AsyncData([...?state.value, newTodo]);
-    } catch (e) {
-      print(e);
     }
   }
 
@@ -103,30 +47,134 @@ class TodoList extends _$TodoList {
     );
   }
 
-  Future<void> updateTodo(String id, {String? title, bool? completed}) async {
+  Future<void> addTodo(
+    String title,
+    String description,
+    String category,
+    List<Map<String, dynamic>> checklist,
+  ) async {
     final supabase = Supabase.instance.client;
     try {
-      final updates = <String, dynamic>{};
-      if (title != null) updates['title'] = title;
-      if (completed != null) updates['completed'] = completed;
+      var userId = 'ad5a4932-19e5-4f97-a4d8-29a6fa0e2c0b';
+      var categoryId = 'fb1778f3-838c-4c2c-8d27-454ef6993b13'; // category
+      var createdAt = DateTime.now().toIso8601String();
+      var updatedAt = DateTime.now().toIso8601String();
+      var startedAt = DateTime.now().toIso8601String();
+      var endedAt =
+          DateTime.now().add(const Duration(days: 7)).toIso8601String();
+      var completed = false;
 
-      final response =
-          await supabase.from('tasks').update(updates).eq('id', id).select();
-      if (response.isEmpty) {
-        return;
+      final taskResponse = await supabase.from('tasks').insert({
+        'user_id': userId,
+        'category_id': categoryId,
+        'title': title,
+        'description': description,
+        'created_at': createdAt,
+        'updated_at': updatedAt,
+        'started_at': startedAt,
+        'ended_at': endedAt,
+        'completed': completed,
+      }).select();
+
+      if (taskResponse.isEmpty) {
+        throw Exception('Failed to insert task');
       }
 
-      state = state.whenData((todos) {
-        return todos.map((todo) {
-          if (todo.id == id) {
+      final taskId = taskResponse.first['id'];
+      final checklistItems = checklist
+          .map((item) => {
+                'task_id': taskId,
+                'title': item['label'],
+                'completed': item['isChecked'],
+              })
+          .toList();
+
+      final checklistResponse =
+          await supabase.from('checklist').insert(checklistItems).select();
+
+      if (checklistResponse.isEmpty) {
+        throw Exception('Failed to insert checklist');
+      }
+
+      final List<Checklist> newChecklist =
+          (checklistResponse as List).map((item) {
+        return Checklist(
+          id: item['id'],
+          taskId: item['task_id'],
+          title: item['title'],
+          completed: item['completed'],
+        );
+      }).toList();
+
+      final newTodo = Todo(
+          id: taskResponse.first['id'],
+          userId: taskResponse.first['user_id'],
+          categoryId: taskResponse.first['category_id'],
+          title: taskResponse.first['title'],
+          description: taskResponse.first['description'],
+          createdAt: DateTime.parse(taskResponse.first['created_at']),
+          updatedAt: DateTime.parse(taskResponse.first['updated_at']),
+          startedAt: DateTime.parse(taskResponse.first['started_at']),
+          endedAt: DateTime.parse(taskResponse.first['ended_at']),
+          completed: taskResponse.first['completed'],
+          checklist: newChecklist);
+
+      state = AsyncData([...?state.value, newTodo]);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> updateCompleted(String id, bool completed, bool isTask) async {
+    final supabase = Supabase.instance.client;
+    try {
+      if (isTask) {
+        // task update
+        final response = await supabase
+            .from('tasks')
+            .update({'completed': completed})
+            .eq('id', id)
+            .select();
+
+        if (response.isEmpty) {
+          return;
+        }
+
+        // Update state with the new completed value
+        state = state.whenData((todos) {
+          return todos.map((todo) {
+            if (todo.id == id) {
+              return todo.copyWith(completed: completed);
+            }
+            return todo;
+          }).toList();
+        });
+      } else {
+        // checklist update
+        final response = await supabase
+            .from('checklist')
+            .update({'completed': completed})
+            .eq('id', id)
+            .select();
+
+        if (response.isEmpty) {
+          return;
+        }
+
+        // Update state with the new completed value for the checklist item
+        state = state.whenData((todos) {
+          return todos.map((todo) {
             return todo.copyWith(
-              title: title ?? todo.title,
-              completed: completed ?? todo.completed,
+              checklist: todo.checklist.map((item) {
+                if (item.id == id) {
+                  return item.copyWith(completed: completed);
+                }
+                return item;
+              }).toList(),
             );
-          }
-          return todo;
-        }).toList();
-      });
+          }).toList();
+        });
+      }
     } catch (e) {
       print(e);
     }
@@ -148,29 +196,26 @@ class TodoList extends _$TodoList {
     }
   }
 
-  void toggleAll(bool completed) {
+  void setChecklist(String todoId, List<Checklist> updatedChecklist) {
     state = state.whenData((todos) {
-      return todos.map((todo) => todo.copyWith(completed: completed)).toList();
+      return todos.map((todo) {
+        if (todo.id == todoId) {
+          return Todo(
+            id: todo.id,
+            userId: todo.userId,
+            categoryId: todo.categoryId,
+            title: todo.title,
+            description: todo.description,
+            createdAt: todo.createdAt,
+            updatedAt: todo.updatedAt,
+            startedAt: todo.startedAt,
+            endedAt: todo.endedAt,
+            completed: todo.completed,
+            checklist: updatedChecklist,
+          );
+        }
+        return todo;
+      }).toList();
     });
-  }
-
-  void removeCompleted() {
-    state = state.whenData((todos) {
-      return todos.where((todo) => !todo.completed).toList();
-    });
-  }
-
-  int get remainingTodos {
-    return state.maybeWhen(
-      data: (todos) => todos.where((todo) => !todo.completed).length,
-      orElse: () => 0,
-    );
-  }
-
-  bool get allComplete {
-    return state.maybeWhen(
-      data: (todos) => todos.every((todo) => todo.completed),
-      orElse: () => false,
-    );
   }
 }
