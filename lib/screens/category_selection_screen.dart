f7dd3ac125fac258a4fccea386687c07e6044aaa
chanw9503 <chanw9503@gmail.com>
2024-08-14 16:35:54 +0900

    Fix. Category Section 위젯 변경사항 수정

--- lib/screens/category_selection_screen.dart
+++ lib/screens/category_selection_screen.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/widgets/category_button.dart';
+import 'package:flutter_todo/widgets/category_button_list.dart';
+import 'package:flutter_todo/widgets/task_card.dart';
 
 class CategorySelectionScreen extends StatelessWidget {
   const CategorySelectionScreen({super.key});
 
-  final List<Map<String, dynamic>> categories = const [
-    {
-      'title': 'Design',
-      'tasks': 5,
-      'category': 'design',
-      'color': '0xFFFFA07A',
-      'icon': Icons.design_services
-    },
-    {
-      'title': 'Learning',
-      'tasks': 3,
-      'category': 'learning',
-      'color': '0xFF87CEFA',
-      'icon': Icons.school
-    },
-    {
-      'title': 'Meeting',
-      'tasks': 1,
-      'category': 'meeting',
-      'color': '0xFFFFD700',
-      'icon': Icons.meeting_room
-    },
+  final List<Map<String, String>> categories = const [
+    {'title': 'Work', 'date': '2024-07-22', 'category': 'work'},
+    {'title': 'Personal', 'date': '2024-07-22', 'category': 'personal'},
+    {'title': 'Study', 'date': '2024-07-22', 'category': 'study'},
   ];
 
   final List<Map<String, dynamic>> todayTasks = const [
@@ -64,14 +47,7 @@ class CategorySelectionScreen extends StatelessWidget {
                     style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                   ),
                   const SizedBox(height: 16),
-                  SingleChildScrollView(
-                    scrollDirection: Axis.horizontal,
-                    child: Row(
-                      children: categories.map((category) {
-                        return _buildCategoryButton(category);
-                      }).toList(),
-                    ),
-                  ),
+                  CategoryButtonList(categories: categories),
                   const SizedBox(height: 32),
                   const Text(
                     "Today's Task",
@@ -80,7 +56,12 @@ class CategorySelectionScreen extends StatelessWidget {
                   const SizedBox(height: 16),
                   Column(
                     children: todayTasks.map((task) {
-                      return _buildTaskCard(task);
+                      return TaskCard(
+                        title: task['title'],
+                        completed: task['completed'],
+                        total: task['total'],
+                        color: task['color'],
+                      );
                     }).toList(),
                   ),
                 ],
@@ -116,56 +97,6 @@ class CategorySelectionScreen extends StatelessWidget {
         onTap: (int index) {
           // Handle tap events here
         },
-      ),
-    );
-  }
-
-  Widget _buildCategoryButton(Map<String, dynamic> category) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: CategoryButton(
-          text: category['title'],
-          icon: category['icon'],
-          onTap: () {},
-          backgroundColor: Color(int.parse(category['color'])),
-          isSelected: false, // Adjust this value as necessary
-          tasks: category.containsKey('tasks') ? category['tasks'] : null,
-          showAddButton: true),
-    );
-  }
-
-  Widget _buildTaskCard(Map<String, dynamic> task) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 8),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: task['color'].withOpacity(0.2),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                task['title'],
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                '${task['completed']} Completed',
-                style:
-                    const TextStyle(fontSize: 14, fontWeight: FontWeight.w600),
-              ),
-            ],
-          ),
-          CircleAvatar(
-            backgroundColor: task['color'],
-            child: Text('${task['total']}'),
-          ),
-        ],
       ),
     );
   }
